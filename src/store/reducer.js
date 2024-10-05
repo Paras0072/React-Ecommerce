56bf1bca80bd85d12fdd2ca93f61c62cc7834dd4
Paras Thakur <parasthakur007241@gmail.com>
2024-10-05 14:22:12 +0530

    Implement cart persistence using localStorage/Redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -40,16 +40,43 @@ const productsSlice = createSlice({
   },
 });
 
+// Create cart slice
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: initialState.cart,
+  reducers: {
+    addToCart: (state, action) => {
+      state.push(action.payload);
+      localStorage.setItem(Cart_storage_key, JSON.stringify(state));
+    },
+    updateCart: (state, action) => {
+      const { id, updatedItem } = action.payload;
+      const index = state.findIndex((item) => item.id === id);
+      if (index !== -1) {
+        state[index] = { ...state[index], ...updatedItem };
+        localStorage.setItem(Cart_storage_key, JSON.stringify(state));
+      }
+    },
+    deleteFromCart: (state, action) => {
+      const id = action.payload;
+      const index = state.findIndex((item) => item.id === id);
+      if (index !== -1) {
+        state.splice(index, 1);
+        localStorage.setItem(Cart_storage_key, JSON.stringify(state));
+      }
+    },
+  },
+});
 
 // Extract action creators from slices
 export const { fetchProducts, createProduct, deleteProduct, updateProduct } =
   productsSlice.actions;
-
+export const { addToCart, updateCart, deleteFromCart } = cartSlice.actions;
 
 // Combine reducers
 const rootReducer = {
   products: productsSlice.reducer,
- 
+  cart: cartSlice.reducer,
 };
 
 export default rootReducer;
